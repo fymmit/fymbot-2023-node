5e49dc5f027b2c47c14d09a755852cd6244c5d49
Timo Blomqvist <blomqvist.timo@gmail.com>
2023-05-26 22:52:41 +0300

    Add image text reader command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,9 +1,11 @@
 import ping from './ping.js';
 import fympix from './fympix.js';
+import readtext from './readtext.js';
 
 const commands = [
     ping,
-    fympix
+    fympix,
+    readtext
 ]
 
 export default commands;
---
+++ src/commands/readtext.ts
@@ -0,0 +1,37 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import { createWorker } from 'tesseract.js';
+
+const data = new SlashCommandBuilder()
+    .setName('readtext')
+    .setDescription('Read text from an image')
+    .addAttachmentOption(option =>
+        option
+            .setName('image')
+            .setDescription('Image to read text from')
+            .setRequired(true));
+
+const execute = async (interaction: ChatInputCommandInteraction) => {
+    await interaction.deferReply();
+    const attachment = interaction.options.getAttachment('image');
+    if (attachment.contentType.split('/')[0] !== 'image') {
+        await interaction.editReply('Not an image.');
+    }
+
+    const worker = await createWorker({});
+
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    await worker.loadLanguage('fin');
+    await worker.initialize('fin');
+    let { data: { text } } = await worker.recognize(attachment.url);
+
+    if (text.length > 2000) {
+        text = text.slice(0, 1900) + '...';
+    }
+
+    await worker.terminate();
+
+    await interaction.editReply(text);
+}
+
+export default { data, execute };
