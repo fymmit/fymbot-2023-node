4d1b1e4ad671eb08eec06c869e4b4fbed72dd768
Timo Blomqvist <blomqvist.timo@gmail.com>
2023-09-14 18:03:21 +0300

    Add support for test env

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,18 +4,25 @@ import commands from './dist/commands/index.js';
 
 config();
 
-const { TOKEN, APP_ID, GUILD_ID } = process.env;
+const {
+	TOKEN,
+	TOKEN_TEST,
+	APP_ID,
+	APP_ID_TEST,
+	GUILD_ID,
+	GUILD_ID_TEST
+} = process.env;
 
 const cmds = commands.filter(c => 'data' in c && 'execute' in c).map(c => c.data.toJSON());
 
-const rest = new REST().setToken(TOKEN);
+const rest = new REST().setToken(TOKEN_TEST ?? TOKEN);
 
 (async () => {
     try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		const data = await rest.put(
-			Routes.applicationGuildCommands(APP_ID, GUILD_ID),
+			Routes.applicationGuildCommands(APP_ID_TEST ?? APP_ID, GUILD_ID_TEST ?? GUILD_ID),
 			{ body: cmds },
 		);
 
